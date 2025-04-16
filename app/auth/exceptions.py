4d60ab5434b0fd4b5fcfabc6d528fa550b8b3bfa
xKuroiUsagix <povoroznikvitalii@gmail.com>
2025-04-16 14:37:18 +0300

    add UserNotFound error

--- app/auth/exceptions.py
+++ app/auth/exceptions.py
@@ -7,3 +7,11 @@ class PermissionsError(HTTPException):
             status_code=status.HTTP_403_FORBIDDEN,
             detail='Not enough permissions for this action'
         )
+
+
+class UserNotFoundError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User not found'
+        )
